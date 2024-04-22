cfe0e0c4d15f66079f3a57d8336bc8721b86aa0c
Romain Philibert <romain.philibert@worldline.com>
2024-04-22 18:02:00 +0200

    fix

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,11 +1,15 @@
 terraform {
   required_providers {
-    hashicups = {
-      source = "worldline/slugify"
+    slugify = {
+      source = "public-cloud-wl/slugify"
       version = "0.1.0"
     }
   }
 }
 provider "slugify" {
   # example configuration here
+}
+
+output "name" {
+  value = provider::slugify::slug("Hello, World!")
 }
