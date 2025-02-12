1318b214de4e57b05d90cf55492efbf6b2981641
Romain Philibert <romain.philibert@worldline.com>
2025-02-12 14:29:47 +0100

    fix tests

--- internal/provider/slug_function_test.go
+++ internal/provider/slug_function_test.go
@@ -8,8 +8,39 @@ import (
 	"testing"
 
 	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+)
+
+const (
+	// providerConfig is a shared configuration to combine with the actual
+	// test configuration so the HashiCups client is properly configured.
+	// It is also possible to use the HASHICUPS_ environment variables instead,
+	// such as updating the Makefile and running the testing through that tool.
+	providerConfig = `
+terraform {
+  required_providers {
+    tools = {
+      source  = "public-cloud-wl/tools"
+      version = "0.1.0"
+    }
+  }
+}
+provider "tools" {
+}
+`
+)
+
+var (
+	// testAccProtoV6ProviderFactories are used to instantiate a provider during
+	// acceptance testing. The factory function will be invoked for every Terraform
+	// CLI command executed to create a provider server to which the CLI can
+	// reattach.
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"tools": providerserver.NewProtocol6WithError(New("test")()),
+	}
 )
 
 func TestExampleFunction_Known(t *testing.T) {
