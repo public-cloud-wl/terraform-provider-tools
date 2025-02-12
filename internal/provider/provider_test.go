9ec0c070cc0fe7381cf5657f2eb979f090aff63f
Romain Philibert <romain.philibert@worldline.com>
2025-02-12 14:38:00 +0100

    fix tests

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -2,13 +2,3 @@
 // SPDX-License-Identifier: MPL-2.0
 
 package provider
-
-import (
-	"testing"
-)
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
-}
