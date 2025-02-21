2be6b82051f18a3420b518533b588b4eb8c81da2
tduverge <tristan.duverger@worldline.com>
2025-02-21 20:58:29 +0100

    feat: check with regex and raise appropriate error

--- internal/provider/idm_group_name_function.go
+++ internal/provider/idm_group_name_function.go
@@ -5,6 +5,7 @@ package provider
 
 import (
 	"context"
+	"regexp"
 	"strings"
 
 	"github.com/gosimple/slug"
@@ -31,12 +32,12 @@ func (r IdmGroupName) Definition(_ context.Context, _ function.DefinitionRequest
 		MarkdownDescription: "Give an application service and a member with format idmrole:[jobrole]@[domain], return the group name associated. If member is not prefixed by idmrole, just return it.",
 		Parameters: []function.Parameter{
 			function.StringParameter{
-				Name:                "as",
-				MarkdownDescription: "The application service.",
-			},
-			function.StringParameter{
 				Name:                "member",
 				MarkdownDescription: "The member to calculate group from. Only member prefix with 'idmrole:' are consider. '*' refer to any people with a role in the application service.",
+			},
+			function.StringParameter{
+				Name:                "as",
+				MarkdownDescription: "The application service.",
 			},
 		},
 		Return: function.StringReturn{},
@@ -44,22 +45,34 @@ func (r IdmGroupName) Definition(_ context.Context, _ function.DefinitionRequest
 }
 
 func (r IdmGroupName) Run(ctx context.Context, req function.RunRequest, resp *function.RunResponse) {
-	var as string
 	var member string
-	var domain string
+	var as string
 	var output string
 
-	resp.Error = function.ConcatFuncErrors(resp.Error, req.Arguments.Get(ctx, &as, &member))
+	resp.Error = function.ConcatFuncErrors(resp.Error, req.Arguments.Get(ctx, &member, &as))
+	// Check if member is prefixed by 'idmrole:' and just return as is if not
 	if !strings.HasPrefix(member, "idmrole:") {
 		resp.Error = function.ConcatFuncErrors(resp.Result.Set(ctx, member))
 		return
 	}
-	role := strings.Split(strings.TrimPrefix(member, "idmrole:"), "@")[0]
-	domain = strings.Split(strings.TrimPrefix(member, "idmrole:"), "@")[1]
+	// Check if member is in format idmrole:[IDM_ROLE]@[DOMAIN]
+	regexMember := "idmrole:[a-zA-Z0-9-_ ]+@[a-zA-Z0-9-_ \\.]+"
+	m, err := regexp.MatchString(regexMember, member)
+	if err != nil {
+		resp.Error = function.ConcatFuncErrors(function.NewFuncError("Unexpected error with regex \\" + regexMember + "\\ : " + err.Error()))
+		return
+	} else if !m {
+		resp.Error = function.ConcatFuncErrors(
+			function.NewFuncError("Member prefixed by 'idmrole:' should be in format 'idmrole:[IDM_ROLE]@[DOMAIN]'\nMember: '" + member + "' doesn't match regex: " + regexMember + " "),
+		)
+		return
+	}
+	role_domain := strings.Split(strings.TrimPrefix(member, "idmrole:"), "@")
+	role, domain := role_domain[0], role_domain[1]
 	if role != "*" {
-		output = "group:" + slug.Make("dl-idm-"+ as + "-" + role) + "@" + domain
+		output = "group:" + slug.Make("dl-idm-"+as+"-"+role) + "@" + domain
 	} else {
-		output = "group:" + slug.Make("dl-idm-" + as) + "@" + domain
+		output = "group:" + slug.Make("dl-idm-"+as) + "@" + domain
 	}
 	output = strings.ReplaceAll(output, "_", "-")
 	resp.Error = function.ConcatFuncErrors(resp.Result.Set(ctx, output))
