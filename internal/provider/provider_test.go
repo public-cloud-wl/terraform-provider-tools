aadba76f8e5ad271ced2b96b19325829aa45d43c
Maarc-D <escasgorille@aol.com>
2025-03-10 15:31:07 +0100

    feat(gcp-dyna-ports-function): implementation of the function

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -2,3 +2,37 @@
 // SPDX-License-Identifier: MPL-2.0
 
 package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+)
+
+const (
+	// providerConfig is a shared configuration to combine with the actual
+	// test configuration so the HashiCups client is properly configured.
+	// It is also possible to use the HASHICUPS_ environment variables instead,
+	// such as updating the Makefile and running the testing through that tool.
+	providerConfig = `
+terraform {
+  required_providers {
+    tools = {
+      source  = "public-cloud-wl/tools"
+      version = "0.1.1"
+    }
+  }
+}
+provider "tools" {
+}
+`
+)
+
+var (
+	// testAccProtoV6ProviderFactories are used to instantiate a provider during
+	// acceptance testing. The factory function will be invoked for every Terraform
+	// CLI command executed to create a provider server to which the CLI can
+	// reattach.
+	testAccDefaultFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"tools": providerserver.NewProtocol6WithError(New("test")()),
+	}
+)
